bf7a788cddb3e194ead2c602712ca178c3ba7bb1
barale2906 <alexanderbarajas@gmail.com>
2024-03-04 23:03:06 -0500

    Csv para cargar unidades

--- database/seeders/UnidadSeeder.php
+++ database/seeders/UnidadSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Ph\Unidad;
+use Exception;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class UnidadSeeder extends Seeder
 {
@@ -13,7 +15,38 @@ class UnidadSeeder extends Seeder
      */
     public function run(): void
     {
-        $un=Unidad::create([
+        $row = 0;
+
+        if(($handle = fopen(public_path() . '/csv/alameda.csv', 'r')) !== false) {
+
+                while(($data = fgetcsv($handle, 26000, ';')) !== false) {
+
+                    $row++;
+
+                    try {
+                        if($data[2]){
+                            $depe=Unidad::where('name', $data[2])->select('id')->first();
+                            $dep=$depe->id;
+                        }else{
+                            $dep=null;
+                        }
+
+                        Unidad::create([
+                            'propiedad_id'  =>1,
+                            'name'       => strtolower($data[0]),
+                            'coeficiente'   =>$data[1],
+                            'unidad_id'     =>$dep,
+                        ]);
+
+                    }catch(Exception $exception){
+                        Log::info('Line: ' . $row . ' alameda with error: ' . $exception->getMessage());
+                    }
+                }
+        }
+
+        fclose($handle);
+
+        /* $un=Unidad::create([
             'propiedad_id'  =>1,
             'name'       => 'torre 3',
         ]);
@@ -23,6 +56,6 @@ class UnidadSeeder extends Seeder
             'name'       => 'Apartamento 506',
             'coeficiente'   =>0.25,
             'unidad_id'     =>$un->id,
-        ]);
+        ]); */
     }
 }
